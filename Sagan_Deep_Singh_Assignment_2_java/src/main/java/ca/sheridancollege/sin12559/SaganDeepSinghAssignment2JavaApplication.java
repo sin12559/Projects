f3d211f0476cd26cb8d1e1952eab4582a731c5bc
sin12559 <134015898+sin12559@users.noreply.github.com>
2023-11-29 00:35:00 -0500

    Add files via upload

---
+++ Sagan_Deep_Singh_Assignment_2_java/src/main/java/ca/sheridancollege/sin12559/SaganDeepSinghAssignment2JavaApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.sin12559;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaganDeepSinghAssignment2JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaganDeepSinghAssignment2JavaApplication.class, args);
+	}
+
+}
